af8c59ca848ac7af22b90548c45f08616c816c4b
nicothebrush <nicola.riolini@gmail.com>
2016-01-17 09:05:46 +0100

    Add import also for price (2 import separated)

--- sql_product_partic/partic.py
+++ sql_product_partic/partic.py
@@ -41,9 +41,9 @@ class micronaet_accounting(osv.osv):
     '''
     _inherit = 'micronaet.accounting'
 
-    # ------------
-    #  TRANSPORT -
-    # ------------
+    # ---------------------
+    #  CODE PRODUCT PARTIC:
+    # ---------------------
     def get_partic_product_partner(self, cr, uid, year=False, context=None):
         ''' Access to anagrafic table of partic productpartner
             Table: tz_prz_cli_art
@@ -66,6 +66,30 @@ class micronaet_accounting(osv.osv):
         except: 
             return False  # Error return nothing
     
+    def get_partic_product_price_partner(self, cr, uid, year=False, 
+            context=None):
+        ''' Access to anagrafic table of partic productpartner
+            Table: tz_prz_cli_art
+        '''
+        table = 'tz_prz_cli_art'
+        if self.pool.get('res.company').table_capital_name(cr, uid, 
+                context=context):
+           table = table.upper()
+
+        cursor = self.connect(cr, uid, year=year, context=context)
+        try:
+            cursor.execute('''
+                SELECT 
+                    CKY_CNT, CKY_ART, CDS_PARTIC_4
+                FROM %s
+                WHERE 
+                    IST_PARTIC = 'P'
+                ORDER BY DTT_INI_VALID desc;
+                ''' % table)
+            return cursor # with the query setted up                  
+        except: 
+            return False  # Error return nothing
+
 class ResPartnerProductPartic(osv.osv):
     ''' Add product partic obj
     '''    
@@ -74,6 +98,7 @@ class ResPartnerProductPartic(osv.osv):
     # -------------------------------------------------------------------------
     #                             Scheduled action
     # -------------------------------------------------------------------------
+    # Import partic description for product-partner:
     def schedule_sql_product_partic_import(self, cr, uid, context=None):
         ''' Import partner product partic
         '''            
@@ -145,6 +170,92 @@ class ResPartnerProductPartic(osv.osv):
             _logger.error('Error generic import partic: %s' % (
                 sys.exc_info(), ))
             return False
-        _logger.info('All transportation is updated!')
+        _logger.info('All partic code is updated!')
+        return True
+        
+    # Import partic price for product-partner:
+    def schedule_sql_product_partic_price_import(self, cr, uid, context=None):
+        ''' Import partner product partic
+        '''
+        try:
+            _logger.info('Start import SQL: partner product price partic')
+            
+            cursor = self.pool.get(
+                'micronaet.accounting').get_partic_product_price_partner(
+                    cr, uid, context=context)
+            if not cursor:
+                _logger.error('Unable to connect, no price partic!')
+                return True
+
+            i = 0            
+            product_pool = self.pool.get('product.product')
+            partner_pool = self.pool.get('res.partner')
+            last = False
+            for record in cursor:
+                i += 1
+                try:
+                    # Map fields:
+                    product_code = record['CKY_ART']
+                    if product_code == last:
+                        continue # old price
+                    else:
+                        last = product_code
+                            
+                    partner_code = record['CKY_CNT']
+                    partner_price = float(
+                        record['CDS_PARTIC_4'].replace(',', '.'))
+                    # TODO import DATE!!! price_from_date price_to_date    
+                    
+                    # Search product:
+                    product_ids =  product_pool.search(cr, uid, [
+                        ('default_code', '=', product_code)], context=context)
+                    if not product_ids:
+                        _logger.error(
+                            'Product code no found (jump): %s' % product_code)
+                        continue    
+
+                    # Search partner:
+                    partner_ids = partner_pool.search(cr, uid, [
+                        '|',
+                        ('sql_customer_code', '=', partner_code),
+                        ('sql_supplier_code', '=', partner_code),
+                        ], context=context)
+                    if not partner_ids:
+                        _logger.error(
+                            'Partner code no found (jump): %s' % partner_code)
+                        continue    
+                       
+                    partic_ids = self.search(cr, uid, [
+                        ('product_id', '=', product_ids[0]),
+                        ('partner_id', '=', partner_ids[0]),                        
+                        ], context=context)
+                    if partic_ids:
+                        if len(partic_ids) > 1:
+                            _logger.info('More than one value: %s' % record)
+                            
+                        self.write(cr, uid, partic_ids, {
+                            'partner_price': partner_price,
+                            }, context=context)    
+                        _logger.info('%s. Update partner price: %s' % (
+                            i, record))
+                    else:                        
+                        self.create(cr, uid, {
+                            'product_id': product_ids[0],
+                            'partner_id': partner_ids[0],
+                            'partner_price': partner_price,
+                            }, context=context)
+                        _logger.info('Create partner price: %s' % record)
+                        partner_pool.write(cr, uid, partner_ids[0], {
+                            'use_partic': True,
+                            }, context=context)                     
+                except:
+                    _logger.error('Error importing price partic [%s]' % (
+                        sys.exc_info(), ))
+                                            
+        except:
+            _logger.error('Error generic import price partic: %s' % (
+                sys.exc_info(), ))
+            return False
+        _logger.info('All partic price is updated!')
         return True
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
